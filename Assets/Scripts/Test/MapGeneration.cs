27ad1ac7faabcdf084ec1574295fc55716d97bc2
OuNiSang <30402947+OuNiSang@users.noreply.github.com>
2023-02-04 18:58:32 -0500

    Add 2D Scene background tiling, apply our 3D surrounding background

--- Assets/Scripts/Test/MapGeneration.cs
+++ Assets/Scripts/Test/MapGeneration.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 using Random = UnityEngine.Random;
 
 public class MapGeneration : MonoBehaviour
 {
     // --------- Public Properties
     public GameObject PlayerObjectReference;
+    public GameObject MapBackgroundReference;
     // The array used to store prefab 
     public GameObject[] PrefabList;
     // For every x unit to generate the next map blocks, which result in the density of blocks 
@@ -17,8 +19,10 @@ public class MapGeneration : MonoBehaviour
     [Range(20, 50)]
     public float MapBlockGenerationDistanceInAdvance = 20;
     // How many Map Block it will try to generate each time 
-    [Range(500, 2000)]
+    [Range(10, 500)]
     public int MapSize;
+    [Range(0, 2)]
+    public float MapAscendingSlope;
     // How many Map Block it will try to generate each time 
     [Range(1, 2)]
     public float MapDensityScale;
@@ -31,6 +35,11 @@ public class MapGeneration : MonoBehaviour
     private float PlayerLatestXPosition;
     private Vector2 MaxPrefabBoundsInList;
 
+
+    private void Awake()
+    {
+        Application.targetFrameRate = -1;
+    }
 
     // --------- Functions
     void Start()
@@ -64,32 +73,53 @@ public class MapGeneration : MonoBehaviour
             Debug.LogError("Cannot Generate Map since no Player Object Reference");
         }
 
+        // Generate Map Block
         for (float i = CurrentPlayerWorldPos.x; i < MapSize; i += DistanceStepForGeneratingMapBlocks)
         {
             GenerateMapBlocksOnPosition(CurrentMapGenerationLocation(i));
         }
 
+        //Generate Map Background
+        if(MapBackgroundReference != null)
+        {
+            MapBackgroundReference.gameObject.transform.localScale =  Vector3.one * MapSize/2;
+            MapBackgroundReference.gameObject.transform.position =  new Vector3(MapSize / 2, MapSize / 2, 50);
+            Instantiate(MapBackgroundReference);
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //PlayerCurrentTransform = PlayerObjectReference.transform;
-        //CurrentPlayerWorldPos = new Vector2(PlayerCurrentTransform.position.x,
-        //                                    PlayerCurrentTransform.position.y);
+       
+    }
 
-        ////Map blocks will only be generated when the step limit is exceeded
-        //if (PlayerCurrentTransform.position.x - PlayerLatestXPosition > DistanceStepForGeneratingMapBlocks)
-        //{
-        //    PlayerLatestXPosition = PlayerCurrentTransform.position.x;
-        //    Vector2 domainPos = CurrentMapGenerationLocation();
+    Mesh GenerateBackgroundMesh()
+    {
+        Mesh backgroundMesh = new Mesh();
+        
+        float mapWidth = MapSize * 1.1f;
+        float mapHight = MapAscendingSlope * MapSize * 1.1f;
+        Vector3 meshStartPos = PlayerCurrentTransform.position + Vector3.one * -20;
+        float depth = -5;
 
-        //    float randomBaseX = Mathf.Abs(ScrambaRangeOfMapBlock.x);
-        //    float randomBaseY = Mathf.Abs(ScrambaRangeOfMapBlock.y);
-        //    GenerateMapBlocksOnPosition(new Vector2( domainPos.x + Random.Range(-randomBaseX, randomBaseX),
-        //                                             domainPos.y + Random.Range(-randomBaseY, randomBaseY)));
-        //}
+
+        backgroundMesh.vertices = new Vector3[] {
+            new Vector3(meshStartPos.x, meshStartPos.y, depth),
+            new Vector3(0, meshStartPos.y + mapHight,depth),
+            new Vector3(meshStartPos.x + mapWidth, meshStartPos.y + mapHight, depth),
+            new Vector3(meshStartPos.x + mapWidth, 0 ,depth)
+        };
+
+        backgroundMesh.uv = new Vector2[] { new Vector2(0, 0), 
+                                            new Vector2(0, 1), 
+                                            new Vector2(1, 1), 
+                                            new Vector2(1, 0) };
+
+        backgroundMesh.triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+
+        return backgroundMesh;
     }
 
     //Based on Univariate Quadratic Equation to find the generate map block location 
@@ -101,7 +131,7 @@ public class MapGeneration : MonoBehaviour
         {
             float xValue = inX + MapBlockGenerationDistanceInAdvance;
 
-            float yValue = 0.7f * inX;         //Equation for map generation line
+            float yValue = MapAscendingSlope * inX;         //Equation for map generation line
 
             returnLocation = new Vector2(xValue, yValue);
         }
