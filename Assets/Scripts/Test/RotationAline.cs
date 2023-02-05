30f49cae88e6da2955527199365c5d2a85de8070
OuNiSang <30402947+OuNiSang@users.noreply.github.com>
2023-02-05 01:45:13 -0500

    Update RotationAline.cs
    Fix the rotation problem

--- Assets/Scripts/Test/RotationAline.cs
+++ Assets/Scripts/Test/RotationAline.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class RotationAline : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class RotationAline : MonoBehaviour
     Vector2 LastPlayer2DWorldUpdatePos;
     Vector2 DeltaPlayer2DWorldDiff;
     Vector2 Player2DWorldInitPos;
-    Vector2 RotationCenterPoint;
+    Vector3 RotationCenterPoint;
 
 
     void Start()
@@ -40,11 +41,9 @@ public class RotationAline : MonoBehaviour
         {
             LastPlayer2DWorldUpdatePos = CurrentPlayer2DWorldPos;
             //Vector3 newHightPos = this.GetComponent<Transform>().position + new Vector3(0, , 0);
-            //float RotateAngle = DeltaPlayer2DWorldDiff.x * RotateDegreePerUnit;
-            //RotateObjectRef.GetComponent<Transform>().RotateAround(RotationCenterPoint, Vector3.up, RotateAngle * Time.deltaTime);
+            float RotateAngle = DeltaPlayer2DWorldDiff.x * RotateDegreePerUnit;
+            RotateObjectRef.GetComponent<Transform>().RotateAround(RotationCenterPoint, Vector3.up, RotateAngle * Time.deltaTime);
         }
-        float RotateAngle = 1 * RotateDegreePerUnit;
-        RotateObjectRef.GetComponent<Transform>().RotateAround(RotationCenterPoint, Vector3.up, RotateAngle * Time.deltaTime);
     }
 
     void GetPlayerUpdate()
@@ -53,5 +52,17 @@ public class RotationAline : MonoBehaviour
         CurrentPlayer2DWorldPos = CurrentPlayerTransform.transform.position;
         CurrentPlayer2DWorldDiff = CurrentPlayer2DWorldPos - Player2DWorldInitPos;
         DeltaPlayer2DWorldDiff = CurrentPlayer2DWorldPos - LastPlayer2DWorldUpdatePos;
+    }
+
+    void MyRotateAround(Vector3 center, Vector3 axis, float angle)
+    {
+        Vector3 pos = RotateObjectRef.transform.position;
+        Quaternion rot = Quaternion.AngleAxis(angle, axis);
+        Vector3 dir = pos - center; //计算从圆心指向摄像头的朝向向量
+        dir = rot * dir;            //旋转此向量
+        RotateObjectRef.transform.position = center + dir;//移动摄像机位置
+        var myrot = RotateObjectRef.transform.rotation;
+        //transform.rotation *= Quaternion.Inverse(myrot) * rot *myrot;//设置角度另一种方法
+        RotateObjectRef.transform.rotation = rot * myrot; //设置角度
     }
 }
